47ac855f410925860c30df57760d6139d0b8154b
CALVlN <=>
2023-11-27 10:24:25 -0800

    part 1 works I think

--- parts.c
+++ parts.c
@@ -17,6 +17,8 @@
 #include<sys/types.h>
 #include<sys/mman.h>
 
+#include <arpa/inet.h>
+
 // Super block
 struct __attribute__((__packed__)) superblock_t {
     uint8_t fs_id[8];
@@ -53,43 +55,89 @@ struct __attribute__((__packed__)) dir_entry_t {
 };
 
 int diskinfo(int argc, char* argv[]) {
-    printf("arg: %s\n", argv[1]);
-
-    int fd;
-    char* data;
-
-    fd = open(argv[1], O_RDWR);
-
+    int fd = open(argv[1], O_RDWR);
     if (fd < 0) {
         printf("device not opened\n");
         exit(1);
     }
 
-    data = mmap(NULL, 0x100, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+    struct stat fileStats;
+    int err = fstat(fd, &fileStats);
+    if (err < 0) {
+        perror("Error getting file information\n");
+        exit(1);
+    }
 
-    printf("%p\n", data);
+    char* dataPtr = mmap(NULL, fileStats.st_size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+    if (dataPtr == MAP_FAILED) {
+        perror("Error mapping file\n");
+        exit(1);
+    }
+
+    // Extract data from file to block, accounting for endianness
     struct superblock_t block = {};
-    memcpy(&block, data, 30);
+    char* blockPtr = dataPtr;
+    memcpy(&block.fs_id, blockPtr, 8);
+    blockPtr += 8;
+    memcpy(&block.block_size, blockPtr, 2);
+    block.block_size = htons(block.block_size);
+    blockPtr += 2;
+    memcpy(&block.file_system_block_count, blockPtr, 4);
+    block.file_system_block_count = htonl(block.file_system_block_count);
+    blockPtr += 4;
+    memcpy(&block.fat_start_block, blockPtr, 4);
+    block.fat_start_block = htonl(block.fat_start_block);
+    blockPtr += 4;
+    memcpy(&block.fat_block_count, blockPtr, 4);
+    block.fat_block_count = htonl(block.fat_block_count);
+    blockPtr += 4;
+    memcpy(&block.root_dir_start_block, blockPtr, 4);
+    block.root_dir_start_block = htonl(block.root_dir_start_block);
+    blockPtr += 4;
+    memcpy(&block.root_dir_block_count, blockPtr, 4);
+    block.root_dir_block_count = htonl(block.root_dir_block_count);
 
-    // HEX OK OR NOT?
+    char* fatEntryData = dataPtr + block.fat_start_block*block.block_size;
+    int freeFatBlocks = 0;
+    int reservedFatBlocks = 0;
+    int allocatedFatBlocks = 0;
+
+    for (int i = 0; i < block.block_size*block.fat_block_count; i += 4) {
+        uint32_t fatData;
+        memcpy(&fatData, fatEntryData + i, 4);
+        fatData = htonl(fatData);
+        if (fatData == 0x00) {
+            freeFatBlocks++;
+        } else if (fatData == 0x01) {
+            reservedFatBlocks++;
+        } else if (fatData <= 0xFFFFFF00) {
+            allocatedFatBlocks++;
+        } else if (fatData == 0xFFFFFFFF) {
+            allocatedFatBlocks++;
+        }
+    }
+
     printf(
-        "Super block information\n"
-        "Block size: %x\n"
-        "Block count: %x\n"
-        "FAT starts: %x\n"
-        "FAT blocks: %x\n"
-        "Root directory starts: %x\n"
-        "Root directory blocks: %x\n"
-        "FAT information\n"
-        "Free blocks: d\n"
-        "Reserved blocks: d\n"
-        "Allocated blocks: d\n",
+        "\nSuper block information\n"
+        "Block size: %d\n"
+        "Block count: %d\n"
+        "FAT starts: %d\n"
+        "FAT blocks: %d\n"
+        "Root directory starts: %d\n"
+        "Root directory blocks: %d\n"
+        "\nFAT information\n"
+        "Free blocks: %d\n"
+        "Reserved blocks: %d\n"
+        "Allocated blocks: %d\n",
         block.block_size,
         block.file_system_block_count,
         block.fat_start_block,
         block.fat_block_count,
         block.root_dir_start_block,
-        block.root_dir_block_count
+        block.root_dir_block_count,
+        freeFatBlocks,
+        reservedFatBlocks,
+        allocatedFatBlocks
     );
 
     close(fd);
